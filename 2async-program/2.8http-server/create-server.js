503953117b4c3bec2af9d8004dcffc6aeec7293e
Poly Titova <poly_titova@mail.ru>
2020-07-08 15:06:51 +0300

    Add html code

--- 2async-program/2.8http-server/create-server.js
+++ 2async-program/2.8http-server/create-server.js
@@ -6,7 +6,44 @@ const http = require(`http`);
 // получение запроса, и отправка ответа
 const onClientConnect = (request, response) => {
   const userAgent = request.headers[`user-agent`];
-  response.end(`Hello, ${userAgent}!`);
+  // html-код
+  const getResponseText = (userAgent) => (`
+    <!Doctype html>
+    <html lang="ru">
+      <head>
+        <title>From Node with love!</title>
+        <link rel="stylesheet" href="style.css">
+      </head>
+      <body>
+        <h1>Привет!</h1>
+        <p>Ты используешь: ${userAgent}.</p>
+      </body>
+    </html>
+  `);
+
+  // стили
+  const styles = `
+  h1 {
+    color: red;
+    font-size: 24px;
+  }
+
+  p {
+    color: green;
+    font-size: 16px;
+    }`;
+
+  // код состояний
+  const HTTP_SUCCESS_CODE = 200;
+  const HTTP_NOT_FOUND_CODE = 404;
+
+  // формирование и отправление заголовока ответа на запрос
+  response.writeHead(HTTP_SUCCESS_CODE, {
+    // чтобы клиент мог определить тип содержимого
+    'Content-Type': `text/html; charset=UTF-8`,
+  });
+
+  response.end(responseText);
 };
 
 // создали переменную для определения порта, на котором будут приниматься подключения
