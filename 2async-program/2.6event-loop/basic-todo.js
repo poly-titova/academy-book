a81555f5a30b860a8aee5f03c733e47304dfba62
Poly Titova <poly_titova@mail.ru>
2020-07-08 11:09:34 +0300

    Add event loop

---
+++ 2async-program/2.6event-loop/basic-todo.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const getBasicTodo = () => {
+  return [
+    `Проснуться`,
+    `Позавтракать`,
+    `Почитать книгу`,
+    `Узнать как работает JavaScript`,
+    `Отдохнуть`,
+  ];
+};
+
+const main = () => {
+  const todos = getBasicTodo();
+  console.log(todos);
+};
+
+main();
