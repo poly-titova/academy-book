1a03c8586776038b6f3bcd819797ea3dad128421
Poly Titova <poly_titova@mail.ru>
2020-10-25 09:51:14 +0300

    Зарегистрировали два одинаковых маршрута

---
+++ 3express/3.5middleware/types-of-middleware/middleware-app-method.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const express = require(`express`);
+const app = express();
+
+const port = 3000;
+app.listen(port);
+
+app.get(`/:id`, (req, res, next) => {
+  const id = +req.params.id;
+  if (id === 100) {
+    res.send(`Hey, superhero!`);
+  } else {
+    next();
+  }
+});
+
+app.get(`/:id`, (req, res) => {
+  const {id} = req.params;
+  res.send(`Your id === ${id}`);
+});
