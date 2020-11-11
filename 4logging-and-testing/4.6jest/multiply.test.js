9ce22302b73f37e3d73099d1607c2e54c1ac69a3
Poly Titova <poly_titova@mail.ru>
2020-11-11 10:54:26 +0300

    Сделали тест с помощью jest и модификтора *.skip

--- 4logging-and-testing/4.6jest/multiply.test.js
+++ 4logging-and-testing/4.6jest/multiply.test.js
@@ -2,6 +2,10 @@
 
 const multiply = require(`./multiply.js`);
 
+test.skip(`multiply -2 by 2 to should be equal -4`, () => {
+  expect(multiply(-2, 2)).toBe(-4);
+});
+
 test.only(`multiply 2 by 2 to should be equal 4`, () => {
   expect(multiply(2, 2)).toBe(4);
 });
