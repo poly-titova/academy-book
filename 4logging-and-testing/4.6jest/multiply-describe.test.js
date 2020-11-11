d876976383fe34f39205827f962c20a4928a2b67
Poly Titova <poly_titova@mail.ru>
2020-11-11 11:25:14 +0300

    Сделали тест с помощью jest и метода describe

---
+++ 4logging-and-testing/4.6jest/multiply-describe.test.js
@@ -0,0 +1,15 @@
+const multiply = require(`./multiply`);
+
+describe(`multiply`, () => {
+  test(`2 by 2 to should be equal 4`, () => {
+    expect(multiply(2, 2)).toBe(4);
+  });
+
+  test(`-2 by 2 to should be equal -4`, () => {
+    expect(multiply(-2, 2)).toBe(-4);
+  });
+});
+
+describe(`other function`, () => {
+  test.todo(`other tests`);
+});
