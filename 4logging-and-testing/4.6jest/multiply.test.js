8666a366d992b7090ff50392c9b2aff05b94a625
Poly Titova <poly_titova@mail.ru>
2020-11-11 10:35:02 +0300

    Сделали тест с помощью jest и метода *.each(table)

--- 4logging-and-testing/4.6jest/multiply.test.js
+++ 4logging-and-testing/4.6jest/multiply.test.js
@@ -5,3 +5,11 @@ const multiply = require(`./multiply.js`);
 test(`multiply 2 by 2 should be equal 4`, () => {
   expect(multiply(2, 2)).toBe(4);
 });
+
+test.each([
+  [1, 1, 1],
+  [1, 2, 2],
+  [2, 1, 2],
+])(`multiply %i by %i should be equal %i`, (a, b, expected) => {
+  expect(multiply(a, b)).toBe(expected);
+});
