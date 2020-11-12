d2a11f60c4d59d03a3dc6d0b6d3f436e8f1b022e
Poly Titova <poly_titova@mail.ru>
2020-11-12 12:53:35 +0300

    Сделали проверку результата ответа

--- 4logging-and-testing/4.9rest-testing/books-routes.e2e.test.js
+++ 4logging-and-testing/4.9rest-testing/books-routes.e2e.test.js
@@ -7,3 +7,9 @@ test(`When get books status code should be 200`, async () => {
   const res = await request(server).get(`/api/books`);
   expect(res.statusCode).toBe(200);
 });
+
+test(`Should had properties id and title`, async () => {
+  const res = await request(server).get(`/api/books/2`);
+  expect(res.body).toHaveProperty(`id`);
+  expect(res.body).toHaveProperty(`title`);
+});
