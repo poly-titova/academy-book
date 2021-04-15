d295d367c489de5a2f80502ff745f723be8c32ba
Poly Titova <poly_titova@mail.ru>
2021-04-15 20:57:39 +0300

    Сделали в случае незаполненной даты рождения вставку даты по умолчанию

--- 6node-and-postgresql/6.8sequelize-hooks/hook.js
+++ 6node-and-postgresql/6.8sequelize-hooks/hook.js
@@ -5,4 +5,14 @@ const { db, sequelize, initDb } = require(`./db`);
 (async () => {
   await db.Author.create({ firstname: `Марио`, lastname: `Пьюзо` });
   await sequelize.close();
+
+  beforeValidate: (author, options) => {
+    if (! author.birthDate) {
+        author.birthDate = new Date(`1990-02-01`);
+    }
+  }
+
+  beforeCreate: (author, options) => {
+    console.log(`Новый автор: ${author.lastname} ${author.birthDate}`);
+  }
 })();
