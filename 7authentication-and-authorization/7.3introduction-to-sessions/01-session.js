1581d994cffd816d33527cdb9abb2e68e8180d36
Poly Titova <poly_titova@mail.ru>
2021-05-15 11:26:57 +0300

    Сделали логику подсчёта посещений

--- 7authentication-and-authorization/7.3introduction-to-sessions/01-session.js
+++ 7authentication-and-authorization/7.3introduction-to-sessions/01-session.js
@@ -2,6 +2,7 @@ const express = require(`express`);
 const expressSession = require(`express-session`);
 
 const PORT = 4000;
+const SECRET_SESSION = `J@2(D#8e33d)`;
 
 const app = express();
 app.use(expressSession({
@@ -12,3 +13,16 @@ app.use(expressSession({
 }));
 
 app.listen(PORT);
+
+app.get(`/`, (req, res) => {
+  let { counter = 0 } = req.session;
+  counter++;
+
+  const welcomeText = `Это ваш первый визит на наш сайт.`;
+  const text = `Вы посетили наш сайт уже ${counter} раз`;
+
+  const message = counter === 1 ? welcomeText : text;
+  req.session.counter = counter;
+
+  res.send(message);
+});
