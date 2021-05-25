48c048448e8ffc1ad7020c39289ce25c6b59160f
Poly Titova <poly_titova@mail.ru>
2021-05-25 10:55:28 +0300

    Ограничили домены, используя настройку origin

---
+++ 7authentication-and-authorization/7.4cors/setting-origin.js
@@ -0,0 +1,12 @@
+const express = require(`express`);
+const path = require(`path`);
+const cors = require(`cors`);
+const app = express();
+const port = 3000;
+
+app.use(cors({
+  origin: `http://www.domainA.com`
+}));
+
+// Ваши настройки
+app.listen(port, () => console.log(`Server listening on port ${port}!`));
