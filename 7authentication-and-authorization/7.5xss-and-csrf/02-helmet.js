79f88a074b47eae08c89d473aebd12517d30159d
Poly Titova <poly_titova@mail.ru>
2021-05-28 11:54:42 +0300

    Применили helmet на практике

---
+++ 7authentication-and-authorization/7.5xss-and-csrf/02-helmet.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const helmet = require('helmet');
+
+const app = express();
+
+app.use(helmet({
+
+  // Конфигурация отдельных middleware
+  contentSecurityPolicy: {
+    directives: {
+      scriptSrc: ["'self'"]
+    }
+  },
+  xssFilter: true,
+}));
