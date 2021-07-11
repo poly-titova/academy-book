0881ab9ddfb1f907d00c4a7138ecc3ba0b25a2ce
Poly Titova <poly_titova@mail.ru>
2021-07-11 14:40:35 +0300

    Сделали получение одной модели

---
+++ php/5.orm-and-db/5.2.getting-started-ActiveRecord/ContactsController.php
@@ -0,0 +1,19 @@
+<?php
+namespace frontend\controllers;
+use frontend\models\Contact;
+use htmlacademy\utils\ContactsImporter;
+use yii\web\Controller;
+
+class ContactsController extends Controller
+{
+    public function actionIndex()
+    {
+        $contact = Contact::find()->one();
+        if ($contact) {
+            print($contact->name);
+            print($contact->phone);
+            print($contact->email);
+            print($contact->position);
+        }
+    }
+}
