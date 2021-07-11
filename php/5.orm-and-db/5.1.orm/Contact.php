e1224eccf36a81be22374d02a4204863438d2083
Poly Titova <poly_titova@mail.ru>
2021-07-11 13:56:19 +0300

    Создали объект контакта

---
+++ php/5.orm-and-db/5.1.orm/Contact.php
@@ -0,0 +1,20 @@
+<?php
+namespace frontend\models;
+use yii\base\Model;
+
+class Contact extends Model
+{
+    public $name;
+    public $phone;
+    public $email;
+    public $position;
+    public function attributeLabels()
+    {
+        return [
+            'name' => 'Имя',
+            'phone' => 'Телефон',
+            'email' => 'Электронная почта',
+            'position' => 'Должность'
+        ];
+    }
+}
