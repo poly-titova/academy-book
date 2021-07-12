0878e2bb34d16797ba4abe027c16b7673bff9e73
Poly Titova <poly_titova@mail.ru>
2021-07-12 13:13:39 +0300

    Сделали связь между компанией и контактом

---
+++ php/5.orm-and-db/5.3links-ActiveRecord-models/Company.php
@@ -0,0 +1,21 @@
+<?php
+namespace frontend\models;
+use yii\db\ActiveRecord;
+
+class Company extends ActiveRecord
+{
+    public function attributeLabels()
+    {
+        return [
+            'name' => 'Имя',
+            'address' => 'Адрес',
+            'email' => 'Рабочий email',
+            'phone' => 'Рабочий телефон',
+            'url' => 'Сайт',
+            'ogrn' => 'ОГРН'
+        ];
+    }
+    public function getContacts() {
+        return $this->hasMany(Contact::class, ['company_id' => 'id'])->inverseOf('company');
+    }
+}
---
+++ php/5.orm-and-db/5.3links-ActiveRecord-models/Contact.php
@@ -0,0 +1,25 @@
+<?php
+namespace frontend\models;
+use yii\db\ActiveRecord;
+
+class Contact extends ActiveRecord
+{
+    public function attributeLabels()
+    {
+        return [
+            'name' => 'Имя',
+            'phone' => 'Телефон',
+            'email' => 'Электронная почта',
+            'position' => 'Должность'
+        ];
+    }
+    public function rules()
+    {
+        return [
+            [['name', 'phone', 'email', 'position'], 'safe']
+        ];
+    }
+    public function getCompany() {
+        return $this->hasOne(Company::class, ['id' => 'company_id']);
+    }
+}
