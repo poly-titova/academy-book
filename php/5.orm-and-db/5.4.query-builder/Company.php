d4dc640ce4e83e18c173ba34844e4e1713d9c8c1
Poly Titova <poly_titova@mail.ru>
2021-07-13 10:44:34 +0300

    Добавили сортировку по умолчанию, а также условие фильтрации контактов

---
+++ php/5.orm-and-db/5.4.query-builder/Company.php
@@ -0,0 +1,24 @@
+<?php
+namespace frontend\models;
+use yii\db\ActiveRecord;
+
+class Company extends ActiveRecord
+{
+    public function attributeLabels()
+    {
+        return [
+            'name' => 'Имя',
+            'address' => 'Адрес',
+            'email' => 'Рабочий email',
+            'phone' => 'Рабочий телефон',
+            'url' => 'Сайт',
+            'ogrn' => 'ОГРН'
+        ];
+    }
+    public function getContacts() {
+        return $this->hasMany(Contact::class, ['company_id' => 'id'])->inverseOf('company');
+    }
+    public function getActiveContacts() {
+        return $this->getContacts()->where(['status' => '1'])->orderBy(['dt_add' => SORT_ASC]);
+    }
+}
