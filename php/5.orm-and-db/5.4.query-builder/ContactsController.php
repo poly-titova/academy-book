6221a01d56b49b6abefdaa14bdbb7ed80642d59c
Poly Titova <poly_titova@mail.ru>
2021-07-13 10:14:12 +0300

    Запросили список контактов с ролью «менеджер» и ограничили выборку 10 результатами

---
+++ php/5.orm-and-db/5.4.query-builder/ContactsController.php
@@ -0,0 +1,20 @@
+<?php
+namespace frontend\controllers;
+use frontend\models\Company;
+use frontend\models\Contact;
+use yii\db\Query;
+use yii\web\Controller;
+
+class ContactsController extends Controller
+{
+    public function actionIndex()
+    {
+        $query = new Query();
+        $query->select(['id', 'name', 'phone'])->from('contact')->where(['position' => 'Менеджер'])->limit(10);
+        // выполнить запрос и получить результат в виде двухмерного массива
+        $rows = $query->all();
+        foreach ($rows as $row) {
+            print($row['name']);
+        }
+    }
+}
